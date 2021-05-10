3d79ccab126b6e189cd0f649269b05656211ed1a
alfredolopez80 <alfredolopez80@gmail.com>
2021-05-10 11:01:10 -0400

    ADD: add new test!!

--- test/test.ts
+++ test/test.ts
@@ -1,4 +1,3 @@
-import { Ownable } from './../typechain/Ownable.d';
 import { ethers } from "hardhat";
 import { Signer } from "ethers";
 import  { expect } from "chai";
@@ -48,14 +47,29 @@ describe("Agreement", function() {
 
 		const Agreements = await agreement.deployed();
 
-		console.log("Agreement Address Deployed: ", Agreements.address);
-
 		console.log("Owner of th Smart Contract:", (await Agreements.owner()).toString());
 		expect(await Agreements.owner()).to.equal(await accounts[0].getAddress());
 		await Agreements.transferOwnership(await accounts[1].getAddress());
 		await Agreements.connect(accounts[1]).renounceOwnership();
 		console.log("New OwnerShip of the Smart Contract:", (await Agreements.owner()).toString());
 		expect(await Agreements.owner()).to.equal('0x0000000000000000000000000000000000000000');
+	});
+
+	//   ** Function partyCreate(), has(), get(), agreements() */
+	//   ** 3. Test Create Agreements and Different Method to Testing : How it is working - Test Case */
+	//   ** t1. Setting a Smart Agreements */
+	//   ** t2. Getting a Smart Agreements by Id, and verify if is valid  */
+	//   ** t3. Getting a Smart Agreements by Id */
+	//   ** t4. Getting a Smart Agreements by Id, through mapping */
+
+	it("3. Test Create Smart Agreement and Testing different way to get and validate", async () => {
+		const Agreement = await ethers.getContractFactory("Agreement");
+		const agreement = await Agreement.deploy();
+
+		const Agreements = await agreement.deployed();
+
+		const party = await accounts[4].getAddress();
+		const counterparty = await accounts[5].getAddress();
 	})
 
 });
